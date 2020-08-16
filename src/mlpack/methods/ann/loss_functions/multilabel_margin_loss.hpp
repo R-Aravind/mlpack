9c73338135f04f29446ecfbe614687d4a09fff48
r-aravind <aravind.ar786@gmail.com>
2020-08-16 07:34:59 -0700

    multilabel_margin_loss: added prereqs and doc headers

---
+++ src/mlpack/methods/ann/loss_functions/multilabel_margin_loss.hpp
@@ -0,0 +1,33 @@
+/**
+ * @file methods/ann/loss_functions/multilabel_margin_loss.hpp
+ * @author R Aravind
+ *
+ * Definition of the Multi-Label Margin Loss function.
+ *
+ * It is a criterion that optimizes a multi-class multi-classification hinge  
+ * loss between input x and output y, both having same shape, with the target 
+ * being a one hot tensor with 1’s at each target class index and 0’s at every 
+ * other index.
+ * 
+ * mlpack is free software; you may redistribute it and/or modify it under the
+ * terms of the 3-clause BSD license.  You should have received a copy of the
+ * 3-clause BSD license along with mlpack.  If not, see
+ * http://www.opensource.org/licenses/BSD-3-Clause for more information.
+ */
+#ifndef MLPACK_ANN_LOSS_FUNCTION_MULTILABEL_MARGIN_LOSS_HPP
+#define MLPACK_ANN_LOSS_FUNCTION_MULTILABEL_MARGIN_LOSS_HPP
+
+#include <mlpack/prereqs.hpp>
+
+namespace mlpack {
+namespace ann /** Artificial Neural Network. **/ {
+
+
+} // namespace ann
+} // namespace mlpack
+
+#endif
+
+
+
+
---
+++ src/mlpack/methods/ann/loss_functions/multilabel_margin_loss_impl.hpp
@@ -0,0 +1,24 @@
+/**
+ * @file methods/ann/loss_functions/multilabel_margin_loss_impl.hpp
+ * @author R Aravind
+ *
+ * Implementation of the Multi-Label Margin Loss function.
+ *
+ * mlpack is free software; you may redistribute it and/or modify it under the
+ * terms of the 3-clause BSD license.  You should have received a copy of the
+ * 3-clause BSD license along with mlpack.  If not, see
+ * http://www.opensource.org/licenses/BSD-3-Clause for more information.
+ */
+#ifndef MLPACK_METHODS_ANN_LOSS_FUNCTION_MULTILABEL_MARGIN_LOSS_IMPL_HPP
+#define MLPACK_METHODS_ANN_LOSS_FUNCTION_MULTILABEL_MARGIN_LOSS_IMPL_HPP
+
+#include "multilabel_margin_loss.hpp"
+
+namespace mlpack {
+namespace ann /** Artificial Neural Network. **/ {
+
+
+} // namespace ann
+} // namespace mlpack
+
+#endif
